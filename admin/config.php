06773770f59c2568b55079135c990e4a5528bb50
lythanhngodev <lythanhngodev@gmail.com>
2017-11-11 11:30:03 +0700

    Cap nhat file
    Cap nhat file

---
+++ admin/config.php
@@ -0,0 +1,5 @@
+<?php 
+	$elearning = array(
+		'HOST' => 'http://localhost/elearning/admin' 
+	);
+ ?>
---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<?php include_once("config.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<base href="<?php echo $elearning['HOST']; ?>/" />
+  	<link rel="stylesheet" type="text/css" href="../bootstrap/dist/css/bootstrap.min.css">
+  	<link rel="stylesheet" type="text/css" href="../bootstrap/dist/css/bootstrap-select.min.css">
+	<link href="../css/vendor/font-awesome.min.css" type="text/css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link href="css/style.css" type="text/css" rel="stylesheet">
+  	<script src="../js/jquery-3.2.1.min.js"></script>
+</head>
+<body>
+	Trang chủ Elearning
+</body>
+</html>
+<script src="nonti/bootstrap-notify.min.js"></script>
